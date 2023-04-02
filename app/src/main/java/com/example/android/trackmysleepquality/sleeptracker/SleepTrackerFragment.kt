cdef53306e6dc32cd62d4c61bec429356016011d
emiliia drahomyretska <90069258+emiliiia@users.noreply.github.com>
2023-04-03 01:34:03 +0300

    Task: Implement GridLayout

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -25,6 +25,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
@@ -116,6 +117,8 @@ class SleepTrackerFragment : Fragment() {
             }
         })
 
+        val manager = GridLayoutManager(activity, 5, GridLayoutManager.HORIZONTAL, false)
+        binding.sleepList.layoutManager = manager
 
         return binding.root
     }
